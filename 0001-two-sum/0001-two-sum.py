a01169bc2f1c2fa17bf69b718c992dfcd01e7ead
AGENTSUBHRA007 <choudhurysayan98@gmail.com>
2026-05-16 16:01:48 +0530

    Time: 0 ms (100%), Space: 20.5 MB (19.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                
-               
-                if target == nums[i] + nums[j]:
-                    return [i, j]
-                    
-        return [-1, -1] 
+        # Dictionary to store numbers we've seen: {number: its_index}
+        seen = {} 
+        
+        # enumerate() gives us both the index (i) and the number itself (num)
+        for i, num in enumerate(nums):
+            
+            # The number we need to find to hit our target
+            needed = target - num
+            
+            # Did we already see the number we need?
+            if needed in seen:
+                # Yes! Return the index of the needed number, and our current index
+                return [seen[needed], i]
+            
+            # No? Add our current number and its index to the dictionary so 
+            # future numbers can find it.
+            seen[num] = i
