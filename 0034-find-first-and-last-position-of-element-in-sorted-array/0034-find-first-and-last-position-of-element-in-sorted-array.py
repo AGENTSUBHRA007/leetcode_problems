ee2603c2cd357ceb4d1ed77a5acf97a76b011f8e
AGENTSUBHRA007 <choudhurysayan98@gmail.com>
2026-05-18 14:41:09 +0530

    Time: 0 ms (100%), Space: 20.5 MB (90.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        # Helper function to find either the leftmost or rightmost target index
+     
         def findBound(is_left: bool) -> int:
             start = 0
             end = len(nums) - 1
@@ -11,11 +11,11 @@ class Solution:
                 mid = (start + end) // 2
                 
                 if nums[mid] == target:
-                    bound = mid # Record this index as a temporary bound
+                    bound = mid 
                     if is_left:
-                        end = mid - 1   # Force search to the LEFT half
+                        end = mid - 1   
                     else:
-                        start = mid + 1 # Force search to the RIGHT half
+                        start = mid + 1 
                 elif nums[mid] < target:
                     start = mid + 1
                 else:
@@ -23,7 +23,7 @@ class Solution:
                     
             return bound
         
-        # Run binary search twice
+       
         first_pos = findBound(is_left=True)
         last_pos = findBound(is_left=False)
         
