bc4e796e795143db6c8837dc12bc869f59a45b13
AGENTSUBHRA007 <choudhurysayan98@gmail.com>
2026-05-18 14:39:45 +0530

    Time: 0 ms (100%), Space: 20.6 MB (27.87%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,19 +1,30 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        a = -1
-        b = -1
         
-        # Loop 1: Find the FIRST occurrence scanning Forward
-        for i in range(len(nums)):
-            if nums[i] == target:
-                a = i
-                break 
+        # Helper function to find either the leftmost or rightmost target index
+        def findBound(is_left: bool) -> int:
+            start = 0
+            end = len(nums) - 1
+            bound = -1
+            
+            while start <= end:
+                mid = (start + end) // 2
                 
-        # Loop 2: Find the LAST occurrence scanning Backward
-        # Corrected range starts at the last index and ends at 0
-        for j in range(len(nums) - 1, -1, -1):
-            if nums[j] == target:
-                b = j 
-                break # Stop immediately since we are traveling backward!
-                
-        return [a, b]
+                if nums[mid] == target:
+                    bound = mid # Record this index as a temporary bound
+                    if is_left:
+                        end = mid - 1   # Force search to the LEFT half
+                    else:
+                        start = mid + 1 # Force search to the RIGHT half
+                elif nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+                    
+            return bound
+        
+        # Run binary search twice
+        first_pos = findBound(is_left=True)
+        last_pos = findBound(is_left=False)
+        
+        return [first_pos, last_pos]
