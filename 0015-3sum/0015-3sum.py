07802e018a9af0986f134c5b3afbcbbc26d96bde
AGENTSUBHRA007 <choudhurysayan98@gmail.com>
2026-05-18 15:24:57 +0530

    Time: 2144 ms (5.01%), Space: 24.2 MB (5.24%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+       
+        triplets = set()
+        nums.sort()
+        
+        for i in range(len(nums)):
+            
+            if nums[i] > 0:
+                break
+                
+            
+            seen = set()
+            
+           
+            for j in range(i + 1, len(nums)):
+              
+                complement = -(nums[i] + nums[j])
+                
+                
+                if complement in seen:
+                    
+                    triplets.add((nums[i], complement, nums[j]))
+                
+               
+                seen.add(nums[j])
+                
+        
+        return [list(t) for t in triplets]
